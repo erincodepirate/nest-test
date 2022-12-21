ecde9da3fca0487834690d46b2244347091b818d
Erin Clark <erincodepirate@gmail.com>
2022-12-21 14:08:24 -0800

    Add examples how to use request and response

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
     @Get()
-    findAll(): string {
+    findAll(
+        //@Req() request: Request,
+        //@Res() response: Response,
+        //@Query() query
+    ): string {
+        //console.log(request);
+        //console.log(response);
+        //console.log(query);
         return 'Find all';
     }
 }
