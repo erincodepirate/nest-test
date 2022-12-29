275fb8d71a66ef757931b26f9eb4c254f95a94d2
Erin Clark <erincodepirate@gmail.com>
2022-12-28 18:27:38 -0800

    Add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
@@ -8,4 +10,19 @@ import { ProductsModule } from './products/products.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
+
+    // only apply to GET
+    //consumer
+    //.apply(LoggerMiddleware)
+    //.forRoutes({path: 'products', method: RequestMethod.GET});
+
+    // exclude GET
+    //consumer
+    //.apply(LoggerMiddleware)
+    //.exclude({path: 'products', method: RequestMethod.GET})
+    //.forRoutes(ProductsController);
+  }
+}
