45d55de2d26a8ffb690707575657deac065ee75d
Erin Clark <erincodepirate@gmail.com>
2023-01-02 18:39:49 -0800

    Add typeorm/mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,28 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest-db',
+      entities: [__dirname + '**/*.entity{.ts,.js}'],
+      synchronize: true
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
+  constructor(private readonly datasource:DataSource) {
+    console.log('connection status', datasource);
+  }
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
 
