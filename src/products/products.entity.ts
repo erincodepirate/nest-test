ee7ef863fb2626927a3a30d5c77a72dbe203dd4c
Erin Clark <erincodepirate@gmail.com>
2023-01-02 18:50:37 -0800

    Add product entity file

---
+++ src/products/products.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({name: 'products'})
+export class ProductsEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 500 })
+    name: string;
+
+    @Column('int')
+    qty: number;
+
+    @Column()
+    price: number;
+}
